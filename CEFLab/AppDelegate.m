4b02fdc63b20f28dd1e250df23d768e991538940
tslearn <tslearn@163.com>
2019-01-19 11:15:05 +0800

    Initial Commit

---
+++ CEFLab/AppDelegate.m
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.m
+//  CEFLab
+//
+//  Created by 田硕 on 2019/1/19.
+//  Copyright © 2019 kztool. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+  // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+  // Insert code here to tear down your application
+}
+
+
+@end
---
+++ CEFLab/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  CEFLab
+//
+//  Created by 田硕 on 2019/1/19.
+//  Copyright © 2019 kztool. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+  return NSApplicationMain(argc, argv);
+}
